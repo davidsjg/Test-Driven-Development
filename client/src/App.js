1628a7fc7e6605929c202c95fdcff26c6fdd1126
Joey Davidson <j.davidson18@gmail.com>
2022-01-19 12:50:49 -0700

    first TTD test passed - data-attribute

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   const [count, setCount] = React.useState(0);
 
   return (
-    <div data-test="component-app" className="App">
-      <h1 data-test="counter-display">
-        The counter is currently&nbsp; 
-        <span data-test="count">{count}</span>
-      </h1>
-      <button
-        data-test="increment-button"
-        onClick={() => setCount(count + 1)}
-      >
-        Increment counter
-      </button>
-    </div>
+    //enzyme has a way to search for attributes with particular values within our shallow wrapper
+    <div data-test="component-app"></div>
   );
 }
 
