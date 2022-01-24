6436509c3affefd4c225de8f0ceb79aac4e4d582
Joey Davidson <j.davidson18@gmail.com>
2022-01-24 09:37:28 -0700

    tests for changing the number of words guessed, including secret word

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -6,14 +6,17 @@ import GuessedWords from "./GuessedWords";
 import Input from "./Input";
 
 function App() {
+  //TODO: get props from shared state
+  const success = false;
+  const secretWord = "party";
+  const guessedWords = [];
+
   return (
     <div data-test="component-app" className="container">
       <h1>Jotto</h1>
       <Congrats success={true} />
-      <GuessedWords
-        guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-      />
-      {/* <Input secretWord={{ secretWord: "party" }} /> */}
+      <Input success={success} secretWord={secretWord} />
+      <GuessedWords guessedWords={guessedWords} />
     </div>
   );
 }
